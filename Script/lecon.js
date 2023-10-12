2809845a3336f8b29e04a4ad884c252527f25f1f
melanie-20 <141557740+melanie-20@users.noreply.github.com>
2023-10-12 16:00:51 +0000

    Add files via upload

---
+++ Script/lecon.js
@@ -0,0 +1,9 @@
+
+const Aire = function(length, width, heigth){
+	let area = 2 *(length * width + length * length + width + length);
+	return area;
+	
+// Calculons l'aire du carré
+
+}
+console.log(Aire(2, 3, 4));
---
+++ Script/lecon2.js
@@ -0,0 +1,7 @@
+const surface_area = function(side_length){
+
+	let surface_area = 6 * side_length ** 2;
+	return side_length
+
+}
+console.log(surface_area(2))
